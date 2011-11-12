08a06f3d7b4c828e0675f7bff53360ca03616dda
Jonathan "Duke" Leto <jonathan@leto.net>
2011-11-12 11:56:56 -0800

    Add a basic test for the login method on the BrowserIDSeed

--- framework/php/classes/seeds/BrowserIDSeed.php
+++ framework/php/classes/seeds/BrowserIDSeed.php
@@ -9,10 +9,10 @@
  * Copyright (c) 2011, CASH Music
  * Licensed under the Affero General Public License version 3.
  * See http://www.gnu.org/licenses/agpl-3.0.html
- *
+ * vim: noexpantab
  **/
 class BrowserIDSeed extends SeedBase {
-	public function __construct($key) {
+	public function __construct() {
 		$this->settings_type = 'com.browserid';
 		$this->connectDB();
 		$this->getCASHSettings();
@@ -22,12 +22,16 @@ class BrowserIDSeed extends SeedBase {
 	// http://openphoto.me
 	public static function login($assertion, $audience)
 	{
-		$ch = curl_init('https://browserid.org/verify');
-		$params = array('assertion' => $assertion, 'audience' => $audience);
+		$verify_url = 'https://browserid.org/verify';
+		$ch         = curl_init($verify_url);
+		$params     = array(
+				'assertion' => $assertion,
+				'audience'  => $audience
+		);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_CAINFO, getConfig()->get('paths')->configs.'/ca-bundle.crt');
+		// curl_setopt($ch, CURLOPT_CAINFO, getConfig()->get('paths')->configs.'/ca-bundle.crt');
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 
--- tests/php/010_BrowserIDSeed.php
+++ tests/php/010_BrowserIDSeed.php
@@ -1,11 +1,26 @@
 <?php
 
+/**
+ * BrowserID Seed Tests http://browserid.org
+ *
+ * @package diy.org.cashmusic
+ * @author CASH Music
+ * @link http://cashmusic.org/
+ *
+ * Copyright (c) 2011, CASH Music
+ * Licensed under the Affero General Public License version 3.
+ * See http://www.gnu.org/licenses/agpl-3.0.html
+ * vim: noexpantab
+ **/
+
 require_once('tests/php/base.php');
 
 class BrowserIDSeedTests extends UnitTestCase {
 	function testBrowserIDSeed(){
-		$id = new BrowserIDSeed($assertion=1,$audience=1);
+		$id = new BrowserIDSeed();
 		$this->assertIsa($id, 'BrowserIDSeed');
+		$rc = $id->login($assertion=1, $audience=1);
+		$this->assertFalse($rc, "invalid assertion returns false");
 	}
 }
 ?>
